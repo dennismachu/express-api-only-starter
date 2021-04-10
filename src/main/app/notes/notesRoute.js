cf3f6ff389bbd62cef9a9996304c187b3d7f9601
dennismachu <dennis.machu@outlook.com>
2021-04-10 22:27:57 +0000

    crud notes

--- src/main/app/notes/notesRoute.js
+++ src/main/app/notes/notesRoute.js
@@ -3,3 +3,25 @@
  * These routes are intended to be stateless, so requests entering 
  * the application through these routes are intended to be authenticated via tokens
  */
+
+ import {
+    create, 
+    readAll, 
+    update, 
+    remove,
+    readOne
+} from './notesController.js'
+
+module.exports = (router) => {
+	router
+		.route('/notes')
+        .post(create)
+        .get(readAll)
+
+        router
+		.route('/notes/:id')
+        .get(readOne)
+        .delete(remove)
+        .patch(update)
+        
+}
