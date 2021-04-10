a8fb8f1c476ab717cfd5ce388b413f06db8ef16e
dennismachu <dennis.machu@outlook.com>
2021-04-10 21:11:27 +0000

    add notes model

--- src/main/app/notes/notesModel.js
+++ src/main/app/notes/notesModel.js
@@ -3,3 +3,30 @@
  * Each database table has a corresponding "Model" which is used to interact with that table. 
  * Models allow you to query for data in your tables, as well as insert new records into the table.
  */
+
+ import mongoose from 'mongoose';
+ import mongoosePaginate from 'mongoose-paginate-v2'
+ 
+ const Schema = mongoose.Schema
+ 
+ const notesSchema = new Schema(
+     {
+         title: {
+             type: String,
+             required: [true, 'Title is required. - Insert a title'],
+             minlength:[3,'Minimun title length 3 characters']
+         },
+         description: {
+             type: String,
+         },
+     },
+     {
+         timestamps: true,
+     }
+ );
+ 
+ notesSchema.plugin(mongoosePaginate);
+ 
+ const notes = mongoose.model('note', notesSchema, 'notes');
+ 
+ export default notes;
