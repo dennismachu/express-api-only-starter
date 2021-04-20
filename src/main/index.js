f4f7adfe8455c561bc45d312de4bfd581f9758d2
thenamskov <ahoryaw1@outlook.com>
2021-04-20 07:32:54 +0000

    updated server port data

--- src/main/index.js
+++ src/main/index.js
@@ -4,15 +4,13 @@ import {database} from './config/database'
 import {app} from './server'
 import {keys} from './config/keys'
 
-if (process.env.NODE_ENV === 'production') {
-  const port = keys.PORT_PRODUCTION || 8080
-}
-const port = keys.PORT_DEVELOPMENT || 3030
+let port = null
+if (process.env.NODE_ENV === 'production') port = keys.PORT_PRODUCTION || 8080
+else port = keys.PORT_DEVELOPMENT || 3030
 
 /**
  * Connect to the Database
  */
-// database()
 module.exports = app.listen(port, async () => {
   await database()
   console.log(`Service listening at http://localhost:${port}`)
